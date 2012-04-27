6609427e0085f3095dd19b335544f3e042d67511
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2012-04-27 18:02:06 +0200

    add our own damn utility belt to the P-A-T-H

--- system/path.bash
+++ system/path.bash
@@ -1,3 +1,6 @@
+# Make use of the tools we brought with us:
+export PATH=$PATH:$DTS/bin
+
 # MySQL
 export PATH=$PATH:/usr/local/mysql/bin
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/mysql/lib
