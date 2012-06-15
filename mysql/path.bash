94cb1e000423912b8b60cd64ce4c1257d9cdc8c8
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2012-06-15 09:24:16 +0200

    separate path.bash's to facilitate incl./exclusion

---
+++ mysql/path.bash
@@ -0,0 +1,2 @@
+export PATH=$PATH:/usr/local/mysql/bin
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/mysql/lib
--- system/path.bash
+++ system/path.bash
@@ -1,9 +1,2 @@
 # Make use of the tools we brought with us:
 export PATH=$PATH:$DTS/bin
-
-# MySQL
-export PATH=$PATH:/usr/local/mysql/bin
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/mysql/lib
-
-# MongoDB
-export PATH=$PATH:/usr/local/mongodb/bin
