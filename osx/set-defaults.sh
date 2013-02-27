9cce47ded6db59c78aa5cdaf947513adbd0ac5a9
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2013-02-27 12:33:04 +0100

    Re-enable full keyboard access by default

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -21,7 +21,7 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
-# defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Disable press-and-hold for keys in favor of key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
